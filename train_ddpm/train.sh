26d904001aeb497cfdd0a157f4f76c4bb7cc733b
SisiGuevara <101356826+SisiGuevara@users.noreply.github.com>
2026-09-08 09:45:51 -0400

    Update training script for new configuration

--- train_ddpm/train.sh
+++ train_ddpm/train.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0;
 
-python main.py --config ./vel_256_512_conditional.yml --exp ./experiments/results/ --doc ./weights/trained_UNet_nn_/ --ni
+python main.py --config ./vel_512_1024_conditional.yml --exp ./experiments/results/ --doc ./weights/trained_UNet_nn_/ --ni
